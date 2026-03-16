90f10966a30c6fa7c4ea2d4d977fd993a7cc1be0
Roland Lenain <roland.lenain@cea.fr>
2026-03-16 11:01:34 +0100

    wip!

--- tests/thm/solver.py
+++ tests/thm/solver.py
@@ -7,7 +7,18 @@ import shutil
 import medcoupling as mc
 import numpy as np
 
-from licocorne import struct, procs
+from licocorne import name_enum, struct, procs, icoco
+
+class OutputField(name_enum.ICoCoNameEnum):
+    T_FUEL = icoco.InputField.T_FUEL
+    D_COOL = icoco.InputField.D_COOL
+    T_COOL = icoco.InputField.T_COOL
+
+
+class InputField(name_enum.ICoCoNameEnum):
+
+    FUEL_POWER = icoco.OutputField.FUEL_POWER
+    WATER_POWER = icoco.OutputField.WATER_POWER
 
 
 class ThmSolver:
@@ -41,15 +52,48 @@ class ThmSolver:
         self._fmap = struct.Fmap(lcm=proc_init.get("Fmap"))
         self._matex = struct.Matex(lcm=proc_init.get("Matex"))
 
-        self._mesh = mc.MEDCouplingCMesh(f"pwr900 mesh")
-        self._mesh.setCoords(mc.DataArrayDouble(np.float64(self._matex.lcm['MESHX']) * 0.01),
-                             mc.DataArrayDouble(np.float64(self._matex.lcm['MESHY']) * 0.01),
-                             mc.DataArrayDouble(np.float64(self._matex.lcm['MESHZ']) * 0.01))
+        # empty the Lifo stack
+        proc_init.clean()
+
+        # allocate 1 parameter value per fuel zone
+        myIntPtr = np.array([2, ], dtype='i')
+        for pname in self._fmap.param.names:
+            myArray = np.resize(self._fmap.param.get_values(pname),
+                                self._fmap.fuel_bundle_nb_per_channel * self._fmap.fuel_channel_nb)
+            if pname in [icoco.OutputField.T_FUEL, icoco.OutputField.D_COOL, icoco.OutputField.T_COOL]:
+                self._fmap.param.set_values(pname, myArray)
+                self._fmap.param.set_type(pname, myIntPtr)
+        self._fmap.lcm.val()
+
+        self._cart_mesh = mc.MEDCouplingCMesh(f"pwr900 mesh")
+        self._cart_mesh.setCoords(mc.DataArrayDouble(np.float64(self._matex.lcm['MESHX']) * 0.01),
+                                  mc.DataArrayDouble(np.float64(self._matex.lcm['MESHY']) * 0.01),
+                                  mc.DataArrayDouble(np.float64(self._matex.lcm['MESHZ']) * 0.01))
+
+        fuel_ids = np.array([0] * self._cart_mesh.getNumberOfCells())
+        for index, zone_type in enumerate(self._fmap.geo_map.cell_type_index):
+            if zone_type in self._matex.fuel_indexes:
+                fuel_ids[index] = 1
+            else:
+                fuel_ids[index] = 0
+
+        mesh = self._cart_mesh.buildUnstructured()
+
+        # valeurs du champ
+        vals = fuel_ids # fuel_mesh.getArray().toNumPyArray().ravel()
+
+        # ids des cellules à garder (valeur non nulle)
+        ids_keep = np.where(vals != 0)[0]
+
+        # convertir en DataArrayInt
+        ids_da = mc.DataArrayInt(ids_keep.tolist())
+
+        # nouveau maillage
+        self._mesh = mesh.buildPartOfMySelf(ids_da)
 
         mc.WriteField(str(self._working_directory / "thm_template.med"),
                     self._create_field("thm_template"),
                     True)
-
 
 
     def _create_field(self, name, nature = mc.IntensiveConservation, values: np.ndarray = None):
